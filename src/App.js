ab6d8bcfaac598d34d86d5e5a5e7440ae20625f7
Ionut Ilie <ionut@leadlion.ro>
2021-07-26 19:16:08 +0300

    upload files

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import textData from "./text";
+
+function App() {
+  const [count, setCount] = useState(4);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+
+    if (count <= 0) {
+      amount = 1;
+      setCount(1);
+    }
+
+    if (count > 8) {
+      amount = 8;
+      setCount(8);
+    }
+
+    setText(textData.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">How many paragraphs?</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+}
+
+export default App;
